c4a0ff2efdad52fe85c4eecf7fa1b7424584421f
RuneIndestegePXL <73280283+RuneIndestegePXL@users.noreply.github.com>
2024-09-28 00:01:04 +0200

    lab 4

--- frontend-web/lab4/customer-app/src/app/core/customers/customer-item/customer-item.component.ts
+++ frontend-web/lab4/customer-app/src/app/core/customers/customer-item/customer-item.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Customer } from '../../../shared/models/customer.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-customer-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './customer-item.component.html',
   styleUrl: './customer-item.component.css'
 })
 export class CustomerItemComponent {
-
+  @Input() customer!: Customer;
+  getDetails(): void{
+    console.log(this.customer);
+  }
 }
