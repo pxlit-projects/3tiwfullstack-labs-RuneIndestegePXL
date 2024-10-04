887433d079b8fd6a5d946d3c8d0c85abed2a1be4
RuneIndestegePXL <rune.indestege@student.pxl.be>
2024-10-04 14:55:23 +0200

    4 binding ex4

--- frontend-web/4Binding/exercise4/src/app/app.component.ts
+++ frontend-web/4Binding/exercise4/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Article } from './Article';
+import { ArticleComponent } from "./article/article.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,10 +16,10 @@ export class AppComponent {
 
   ngOnInit() {
     this.articles = [
-      { id: 1, name: 'Tandenborstel', purchasePrice: 1.5, salePrice: 3.24, imageUrl: 'assets/images/toothbrush.png' },
-      { id: 2, name: 'Handdoek', purchasePrice: 2.5, salePrice: 4.75, imageUrl: 'assets/images/towel.png' },
-      { id: 3, name: 'Washandje', purchasePrice: 0.4, salePrice: 1.2, imageUrl: 'assets/images/facecloth.png' },
-      { id: 4, name: 'Keukenrol', purchasePrice: 1.75, salePrice: 0.65, imageUrl: 'assets/images/kitchenroll.png' }
+      { id: 1, name: 'Tandenborstel', purchasePrice: 1.5, salePrice: 3.24, imageUrl: 'toothbrush.png' },
+      { id: 2, name: 'Handdoek', purchasePrice: 2.5, salePrice: 4.75, imageUrl: 'towel.png' },
+      { id: 3, name: 'Washandje', purchasePrice: 0.4, salePrice: 1.2, imageUrl: 'facecloth.png' },
+      { id: 4, name: 'Keukenrol', purchasePrice: 1.75, salePrice: 0.65, imageUrl: 'kitchenroll.png' }
     ];
   }
 }
--- frontend-web/4Binding/exercise4/src/app/article/article.component.ts
+++ frontend-web/4Binding/exercise4/src/app/article/article.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../Article';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
+  @Input() articles!: Article[];
 
+  showDetails(article: Article) {
+    alert(`Item description: ${article.name} - Sale Price: €${article.salePrice} - Purchase Price: €${article.purchasePrice}`);
+  }
 }
