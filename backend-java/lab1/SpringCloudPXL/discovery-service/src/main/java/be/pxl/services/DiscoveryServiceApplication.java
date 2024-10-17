a750f0f9b07084bbec6a2f277dc0cd136c5c1e4e
RuneIndestegePXL <rune.indestege@student.pxl.be>
2024-10-17 11:42:13 +0200

    discovery working

--- backend-java/lab1/SpringCloudPXL/discovery-service/src/main/java/be/pxl/services/DiscoveryServiceApplication.java
+++ backend-java/lab1/SpringCloudPXL/discovery-service/src/main/java/be/pxl/services/DiscoveryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Discovery Service
+ * Hello world!
  *
  */
 @SpringBootApplication
